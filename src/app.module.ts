d6f360a6e550c8a940d1d6ce7f60f835c356241a
Otakawai <quentin.detre4254@gmail.com>
2022-10-20 15:23:48 +0200

    Back

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthorModule } from './author/author.module';
-import { ArticleModule } from './article/article.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [AuthorModule, ArticleModule],
   controllers: [AppController],
   providers: [AppService],
+  imports: [UserModule],
 })
 export class AppModule {}
