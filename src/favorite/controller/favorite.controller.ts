b7aa4fe80fa2143592db4a56f985eb1e002cc82c
Otakawai <quentin.detre4254@gmail.com>
2022-12-13 18:32:36 +0100

    Fixed (normally)
    should work

--- src/favorite/controller/favorite.controller.ts
+++ src/favorite/controller/favorite.controller.ts
@@ -1,6 +1,7 @@
 import { FavoriteDto } from '../favorite.dto/favorite.dto';
 import { FavoriteService } from '../service/favorite.service';
 import { Body, Controller, Get, Post, Put, Delete, Param } from '@nestjs/common';
+import { UserDto } from 'src/user/dto/user.dto';
 
 
 @Controller('favorite')
--- src/favorite/service/favorite.service.ts
+++ src/favorite/service/favorite.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { userInfo } from 'os';
 import { UserDto } from 'src/user/dto/user.dto';
-import { Repository } from 'typeorm';
+import { Repository, QueryBuilder, DataSource } from 'typeorm';
 import { FavoriteDto } from '../favorite.dto/favorite.dto';
 
 @Injectable()
@@ -16,10 +16,20 @@ export class FavoriteService {
     async getFavorite():Promise<FavoriteDto[]> {
         return this.favoriteRepository.find();
     }
-    async getAllFavoriteFromUser(user: UserDto):Promise<FavoriteDto[]> {
-        return await this.favoriteRepository.find({
-            where: [{"user": user}]
-        });
+    async getAllFavoriteFromUser(user: number):Promise<FavoriteDto[]> {
+        console.log(user);
+        const thisUser = this.favoriteRepository
+        .createQueryBuilder()
+        .where("FavoriteDto.user = :id", {id: user})
+        console.log(thisUser.getParameters());
+        const result = await thisUser.execute();
+        console.log(result)
+        return result;
+        // const getUser = await this.favoriteRepository.findOne({ select: ["user"]});
+        // return await this.favoriteRepository.find({
+        //     relations: ["articles"],
+        //     where: [getUser == thisUser]
+        // });
     }
 
     async updateFavorite(favorite : FavoriteDto) {
@@ -30,3 +40,4 @@ export class FavoriteService {
         this.favoriteRepository.delete(favorite);
     }
 }
+
